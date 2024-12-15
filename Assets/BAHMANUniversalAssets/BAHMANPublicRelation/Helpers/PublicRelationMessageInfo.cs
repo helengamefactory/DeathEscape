1c4d1b28744e3b4b69adbfe91efe9e0d4834fc6f
Bahman Moazzen <bahmanmoazzen@gmail.com>
2024-12-15 09:55:25 +0330

    Done Removing Android References

--- Assets/BAHMANUniversalAssets/BAHMANPublicRelation/Helpers/PublicRelationMessageInfo.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-[CreateAssetMenu(fileName ="NewMessage",menuName = "BAHMAN Unity Assets/Public Relation/New Message",order =2)]
-public class PublicRelationMessageInfo : ScriptableObject
-{
-    public string Title;
-    public string Message;
-}
--- Assets/BAHMANUniversalAssets/BAHMANPublicRelation/Helpers/PublicRelationPageCountSettingInfo.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-[CreateAssetMenu(fileName = "NewMarketIntentSetting", order = 3, menuName = "BAHMAN Unity Assets/Public Relation/New Page Count Settings")]
-public class PublicRelationPageCountSettingInfo : ScriptableObject
-{
-    public PublicRelationPageCountSetting[] _PageCountSettings;
-}
-
-
